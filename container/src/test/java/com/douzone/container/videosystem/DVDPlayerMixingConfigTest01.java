1605e0d4cf0b35d7abfbc63785e8a082cec750af
inhyo <inhyo825@gmail.com>
2020-03-24 15:59:04 +0900

    Mixing Config

--- container/src/test/java/com/douzone/container/videosystem/DVDPlayerMixingConfigTest01.java
+++ container/src/test/java/com/douzone/container/videosystem/DVDPlayerMixingConfigTest01.java
@@ -1,6 +1,5 @@
 package com.douzone.container.videosystem;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Rule;
@@ -8,39 +7,23 @@ import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import config.videosystem.DVDPlayerConfig;
+import config.videosystem.mixing.DVDPlayerConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = DVDPlayerConfig.class)
-public class DVDPlayerJavaConfigTest {
+public class DVDPlayerMixingConfigTest01 {
 	@Rule
 	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
-
+	
 	@Autowired
-	private DigitalVideoDisc dvd;
-
-	@Autowired
-	// @Qualifier("dvdPlayer03") // 설정 클래스 Bean 생성 메소드 이름
-	@Qualifier("player04") // @Bean의 name 값
 	private DVDPlayer player;
-
-	@Test
-	public void testDVDNotNull() {
-		assertNotNull(dvd);
-	}
-
+	
 	@Test
 	public void testPlayerNotNull() {
 		assertNotNull(player);
 	}
 
-	@Test
-	public void testPlay() {
-		player.play();
-		assertEquals("Playing Movie MARVEL's Avengers", systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
-	}
 }
